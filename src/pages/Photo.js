a3cc683e35e5db02e7c5651b7c051e79860c2605
dding-v <v.dding.v@gmail.com>
2023-02-05 23:20:26 +0900

    Photo 구현

--- src/pages/Photo.js
+++ src/pages/Photo.js
@@ -1,14 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import classes from "./Menu.module.css";
+import classes from "./Photo.module.css";
 
 import Card from "../components/Card";
+
+import write from '../img/write-01.png'
+import faceNo from '../img/face-no.png'
+import faceYes from '../img/face-yes.png'
+import fighting from '../img/fighting-01.png'
+
 
 const Photo = () => {
   return (
     <Card>
       <div className={classes.main}>
+        <img src={write} className={classes.img} />
+        <img src={fighting} className={classes.img} />
+        <img src={faceNo} className={classes.img} />
+        <img src={faceYes} className={classes.img} />
       </div>
     </Card>
   );
